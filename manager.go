a9233a61d438514ba56b2d2b065b65476b363e40
Matt Layher <mdlayher@gmail.com>
2014-04-05 18:28:45 -0400

    Add request and response channel skeleton

--- manager.go
+++ manager.go
@@ -4,12 +4,29 @@ import ()
 
 // manager is reponsible for coordinating the application and handling the main event loop
 func manager(killChan chan bool, exitChan chan int) {
+	// Channels to handle incoming requests and outgoing responses
+	reqChan := make(chan []byte, 1000)
+	resChan := make(chan []byte, 1000)
+
 	// Loop and handle events
 	for {
 		select {
 		// Stop the application
 		case <-killChan:
+			// Close manager channels
+			close(reqChan)
+			close(resChan)
+
+			// Trigger graceful shutdown
 			exitChan <- 0
+			break
+		// Handle incoming requests
+		case request := <-reqChan:
+			_ = request
+			break
+		// Handle outgoing responses
+		case response := <-resChan:
+			_ = response
 			break
 		}
 	}
