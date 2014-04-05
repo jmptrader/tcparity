0d033e9aa0e707aa3eccab447a462180925dd1f9
Matt Layher <mdlayher@gmail.com>
2014-04-05 18:01:14 -0400

    Initial commit of skeleton main

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// app is the name of the application, as printed in logs
+const app = "tcparity"
+
+// test is a flag which causes tcparity to start, and exit shortly after
+var test = flag.Bool("test", false, "Make tcparity start, and exit shortly after. Used for testing.")
+
+func main() {
+	// Set up command line options
+	flag.Parse()
+
+	// If test mode, trigger quit shortly after startup
+	// Used for CI tests, so that we ensure tcparity starts up and is able to stop gracefully
+	if *test {
+		go func() {
+			fmt.Println(app, ": launched in test mode")
+			time.Sleep(5 * time.Second)
+
+			fmt.Println(app, ": test mode triggering graceful shutdown")
+			err := syscall.Kill(os.Getpid(), syscall.SIGTERM)
+			if err != nil {
+				fmt.Println(app, ": failed to invoke graceful shutdown, halting")
+				os.Exit(1)
+			}
+		}()
+	}
+
+	// Launch manager via goroutine
+	killChan := make(chan bool, 1)
+
+	// Gracefully handle termination via UNIX signal
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	for sig := range sigChan {
+		// Trigger manager shutdown
+		fmt.Println(app, ": caught signal:", sig)
+		killChan <- true
+		break
+	}
+
+	// Force terminate if signaled twice
+	go func() {
+		for sig := range sigChan {
+			fmt.Println(app, ": caught signal:", sig, ", force halting now!")
+			os.Exit(1)
+		}
+	}()
+
+	// Graceful exit
+	fmt.Println(app, ": graceful shutdown complete")
+	os.Exit(0)
+}
